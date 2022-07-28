ea68d4c405674db2ecb7fe6bafaf50ff5aa931be
Mateusz Hallala <matt@huncwot.com>
2022-07-28 22:24:03 +0200

    first lessons

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello mati!")
+
+
+	name := "mati"
+	var verb string = "to"
+	var mati = "ciul"
+
+	fmt.Println(name, verb, mati)
+
+	num1 := 3
+	num2 := 17
+
+	fmt.Println(num1 * num2)
+}
